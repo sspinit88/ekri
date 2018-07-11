b1d23412c50db15e4797c11361c7034e9b682bc2
Sergey Pashnin <sspinit88@gmail.com>
2018-07-12 01:21:00 +0300

    main+

---
+++ js/ekriScript.js
@@ -0,0 +1,41 @@
+
+/*--headerMenu--*/
+$(function () {
+    var elem =$('.headerMenu__li');
+    elem.hover(
+        function () {
+            $(this).find('.subMenu').stop(true).fadeIn(500);
+            $(this).find('.headerMenu__link').addClass('headerMenu__link--active');
+
+        },
+        function () {
+            $(this).find('.subMenu').stop(true).fadeOut(300);
+            $(this).find('.headerMenu__link').removeClass('headerMenu__link--active');
+        }
+    ); // hover
+}); // fu()
+
+
+
+/*--mainSlider--*/
+$(document).ready(function () {
+    $('.mainSlider').slick({
+        dots: false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        speed: 1700,
+        fade: true,
+        zIndex: 10,
+        arrows: false,
+        // responsive: [
+        //      {
+        //          breakpoint: 1000,
+        //          settings: {
+        //              infinite: true,
+        //              dots: false,
+        //              arrows: false
+        //          }
+        //      }
+        //  ]
+    });
+});
