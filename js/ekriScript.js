a03be4290fa5ea613ff911cd666515162793b972
Sergey Pashnin <sspinit88@gmail.com>
2018-07-11 19:22:27 +0300

    menu+

--- js/ekriScript.js
+++ js/ekriScript.js
@@ -4,12 +4,12 @@ $(function () {
     var elem =$('.headerMenu__li');
     elem.hover(
         function () {
-            $(this).find('.subMenu').stop(true).slideDown(500);
+            $(this).find('.subMenu').stop(true).fadeIn(500);
             $(this).find('.headerMenu__link').addClass('headerMenu__link--active');
 
         },
         function () {
-            $(this).find('.subMenu').stop(true).slideUp(0);
+            $(this).find('.subMenu').stop(true).fadeOut(300);
             $(this).find('.headerMenu__link').removeClass('headerMenu__link--active');
         }
     ); // hover
